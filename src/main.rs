57a5eb3ae37614ec15bbb89d742d9998761559b3
Saksham Gupta <sakshamdam54321@gmail.com>
2024-06-02 03:21:16 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use colored::Colorize;
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let mut commands: HashMap<String, fn(&str, &[&str]) -> u8> = HashMap::new();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    commands.insert(String::from("hello"), |_arg0, _args| {
+        println!("Hello, World!");
+        return 0;
+    });
+
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let mut split = input.split_whitespace();
+
+        let Some(arg0) = split.next() else {
+            continue;
+        };
+
+        let rest: Vec<_> = split.collect();
+
+        if let Some(cmd) = commands.get(arg0) {
+            cmd(arg0, &rest);
+        } else {
+            println!("{}: {} not found", arg0, "command".red());
+        }
+
+        dbg!(input);
+    }
 }
